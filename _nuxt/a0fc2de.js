03e36ac1cd91673c445e916edcf80e3f63b28176
Vadim Ignatov <ignatov0131@gmail.com>
2022-05-25 14:33:33 +0300

    Add nuxt files

---
+++ _nuxt/a0fc2de.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{321:function(e,n,t){"use strict";t.r(n);var r={name:"Index",beforeCreate:function(){this.$router.push("/sport")}},o=t(39),component=Object(o.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
